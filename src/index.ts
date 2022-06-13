3ee2078aa8c0a411fe270c9b7a5f44f5d6658ff5
David Kanders <kanders.davids@gmail.com>
2022-06-13 06:19:50 +0300

    add package exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+// # APICore
 export { APICore } from './APICore/APICore'
+
+// # types
+export * from './types'
+export * from './types/APICore'
+
+// # utils
+export { is2XX, is3XX, useBatches } from './utils'
